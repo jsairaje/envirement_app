a30eb2bd8d986a019224201bcc12152b9f4b044c
shoyeb <2020BTECS00059@walchandsangli.ac.in>
2023-04-04 17:01:46 +0530

    logo and firebase connectivity

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -7,68 +8,76 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  String email = '';
+  String passwd = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Container(
-            margin: EdgeInsets.only(left: 25 ,right: 25 ,top: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-
-              children: [
-                Text("Login",
+        child: Container(
+          margin: EdgeInsets.only(left: 25, right: 25, top: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Login",
                 style: TextStyle(
                   fontSize: 45,
                 ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
                 ),
-                SizedBox(height: 20,),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-
-                    hintText: 'abc@gmail.com',
+                onChanged: (value) {
+                  email = value;
+                  print(email);
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+                onChanged: (value) {
+                  print("jnn");
+                  passwd = value;
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 45,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    // primary: Colors.brown.shade600,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
                   ),
-                ),
-                SizedBox(height: 20,),
-                TextField(
-
-                  decoration: InputDecoration(
-
-                    hintText: 'password',
-                  ),
-                ),
-                SizedBox(height: 20,),
-                SizedBox(
-                  width: double.infinity,
-                  height: 45,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.brown.shade600,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)
-                      ),
-
+                  onPressed: () {
+                    print(email + " " + passwd);
+                    Navigator.pushNamed(context, 'envconst');
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
                     ),
-                    onPressed: (){
-                      Navigator.pushNamed(context, 'envconst');
-                    },
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
                   ),
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
         ),
-
+      ),
     );
   }
 }
-
-
-
