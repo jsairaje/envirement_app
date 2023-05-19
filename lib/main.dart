cafea8bd6af24023a985391fa6771a814b8bf8e8
sairaje <jsairaje@gmail.com>
2023-05-19 18:19:41 +0530

    19-05-23 second change

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:portfolio/envconst.dart';
-import 'package:portfolio/environment.dart';
-import 'package:portfolio/restoration.dart';
+import 'package:portfolio/services/envconst.dart';
+import 'package:portfolio/services/environment.dart';
+import 'package:portfolio/services/restoration.dart';
 import 'package:portfolio/home.dart';
-import 'package:portfolio/login.dart';
+import 'package:portfolio/auth/login.dart';
 // import 'package:portfolio/signup.dart';
 import 'package:flutter/material.dart';
-import 'package:portfolio/thirdsection.dart';
+import 'package:portfolio/services/thirdsection.dart';
 import 'package:portfolio/Admin/admin.dart';
 void main() {
   runApp(MaterialApp(
